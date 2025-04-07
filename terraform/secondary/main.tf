d9140bdbf0faa2e563c1cc3a8980907921833cbc
Pavlo Ponomarenko <reactor2345@gmail.com>
2025-04-07 21:59:09 +0300

    implement vpc, public network, ec2 for cloud-gallery

--- terraform/secondary/main.tf
+++ terraform/secondary/main.tf
@@ -1,3 +1,18 @@
+resource "tls_private_key" "my_key" {
+  algorithm = "RSA"
+  rsa_bits  = 2048
+}
+
+resource "local_file" "private_key" {
+  filename        = "ssh-key.pem"
+  content         = tls_private_key.my_key.private_key_pem
+}
+
+resource "aws_key_pair" "ssh_key" {
+  key_name   = "ssh-key"
+  public_key = tls_private_key.my_key.public_key_openssh
+}
+
 resource "aws_vpc" "main_vpc" {
   cidr_block = "10.0.0.0/16"
 
@@ -52,6 +67,13 @@ resource "aws_security_group" "allow_port_5000" {
   vpc_id = aws_vpc.main_vpc.id
 
   ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     from_port   = 5000
     to_port     = 5000
     protocol    = "tcp"
@@ -67,5 +89,35 @@ resource "aws_security_group" "allow_port_5000" {
 
   tags = {
     Name = "AllowPort5000"
+  }
+}
+
+resource "aws_instance" "cloud_gallery" {
+  ami           = "ami-0ecf75a98fe8519d7"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public_subnet.id
+  vpc_security_group_ids = [
+    aws_security_group.allow_port_5000.id
+  ]
+
+  key_name = aws_key_pair.ssh_key.key_name
+
+  associate_public_ip_address = true
+
+  user_data = <<-EOF
+              #!/bin/bash
+
+              yum update -y
+              yum install -y git
+              yum install -y python3
+              yum install pip
+              pip3 install flask
+              git clone https://github.com/Pavlo-Ponomarenko/cloud-gallery.git
+              cd cloud-gallery
+              python3 -m flask --app App run --host=0.0.0.0
+            EOF
+
+  tags = {
+    Name = "Cloud Gallery"
   }
 }
